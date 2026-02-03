6395e8242230d77f9e0dfa105018f32cf96a9421
Killian Morere <killian.morere@fms-ea.com>
2026-02-03 08:40:32 +0100

    Implemented AddCourse in Admin to post a new course in the json file.

--- src/app/components/admin/admin.ts
+++ src/app/components/admin/admin.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { Auth } from '../../service/auth';
 import { Router } from '@angular/router';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Training } from '../../model/Training';
 
 @Component({
     selector: 'app-admin',
@@ -9,11 +11,24 @@ import { Router } from '@angular/router';
     styleUrl: './admin.css',
 })
 export class Admin implements OnInit {
-    constructor(private auth: Auth, private router: Router) { }
+    constructor(private auth: Auth, private router: Router, private http: HttpClient) { }
 
     ngOnInit(): void {
         if (this.auth.isLoggedIn() === false) {
-            this.router.navigateByUrl("trainings");
+            /*this.router.navigateByUrl("trainings");*/
         }
+
+        this.addCourse(new Training(
+            4,
+            "Test",
+            "Testing",
+            1250,
+            1
+        ));
+    }
+
+    addCourse(course: Training) {
+        this.http.post("http://localhost:3000/courses", JSON.stringify(course));
+        console.log(JSON.stringify(course));
     }
 }
